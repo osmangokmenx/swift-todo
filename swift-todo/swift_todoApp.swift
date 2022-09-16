3ca7b0c2a35d5cfb114fc07393835f5cfccd62a4
osmangokmenx <52015838+osmangokmenx@users.noreply.github.com>
2022-09-16 10:05:42 +0300

    Initial Commit

---
+++ swift-todo/swift_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_todoApp.swift
+//  swift-todo
+//
+//  Created by Osman Gökmen on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct swift_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
