4e95beb54ebee18305f6a323b2bfd5866653e91e
osmangokmenx <52015838+osmangokmenx@users.noreply.github.com>
2022-09-16 20:50:51 +0300

    todo add & bottomSheet

--- swift-todo/ContentView.swift
+++ swift-todo/ContentView.swift
@@ -7,46 +7,73 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
-    @State private var showAlert : Bool = false
     
+    @State private var showAlert : Bool = false
+    @State private var newTodo : String = ""
     @State var ToDoItems: [String] = [
         "Swift öğren",
         "Swift Yaz",
         "Swift"
     ]
+    
     var body: some View {
-        VStack {
-            List {
-                ForEach(ToDoItems, id: \.self) { item in
-                    ListItemRow(title: item)
-                }.onDelete(perform: delete)
-                
-            } .navigationTitle("Todo")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        EditButton();
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button("Tap to show alert") {
-                            showAlert = true
-                        }.alert(isPresented: $showAlert) {
-                            Alert(
-                                title: Text("Current Location Not Available"),
-                                message: Text("Your current location can’t be " +
-                                            "determined at this time.")
-                            )
+        NavigationView {
+            
+            VStack {
+                List {
+                    ForEach(ToDoItems, id: \.self) { item in
+                        ListItemRow(title: item)
+                    }.onDelete(perform: delete)
+                } .navigationTitle("Todo")
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            EditButton();
                         }
-                        //                        NavigationLink("add", destination: Text("asd"))
-                        
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button("Add") {
+                                showAlert = true
+                            }.sheet(isPresented: $showAlert,
+                                    onDismiss: didDismiss ) {
+                                VStack(alignment: .center, spacing: 27.0) {
+                                    Spacer()
+                                    
+                                    TextField(
+                                        "User name (email address)",
+                                        text: $newTodo
+                                    ).padding().frame(width: 200, height: 40).border(Color.blue).fixedSize()
+                                    Button("Save") {
+                                        save()
+                                    }.padding(.top, 2.0)
+                                    Spacer()
+                                    
+                                }
+                                
+                            }
+                        }
                     }
-                }
+                
+            }
         }
     }
+    
     func delete (at offsets: IndexSet) {
         if let first = (offsets.first) {
             ToDoItems.remove(at: first)
         }
+    }
+    
+    func save () {
+        ToDoItems.append(newTodo)
+        newTodo = ""
+        showAlert = false
+    }
+    
+    
+    func didDismiss () {
+        showAlert = false
     }
 }
 
@@ -54,9 +81,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            ContentView()
-        }
+        ContentView()
     }
 }
 
---
+++ swift-todo/CreateTodo.swift
@@ -0,0 +1,22 @@
+//
+//  CreateTodo.swift
+//  swift-todo
+//
+//  Created by Osman Gökmen on 16.09.2022.
+//
+
+import SwiftUI
+
+struct CreateTodo: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct CreateTodo_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateTodo()
+    }
+}
